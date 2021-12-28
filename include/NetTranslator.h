3995cfdbb8236e95d9453a7364142a61ec9ec87a
Dhruv Srivastava <dhruvdhs.ds@gmail.com>
2021-12-28 15:16:47 +0530

    restructure

---
+++ include/NetTranslator.h
@@ -0,0 +1,49 @@
+// enum class PieceOwner : uint32_t
+// {
+// 	White,
+// 	Black
+// };
+
+// enum class Pieces : uint32_t
+// {
+// 	King,
+// 	Queen,
+// 	Rook,
+// 	Bishop,
+// 	Knight,
+// 	Pawn
+// };
+
+enum class GameMessage : uint32_t
+{
+	
+	GetStatus,//0 
+	Ping,
+	YourTurn, //2
+	
+	Client_Accepted,
+	Client_AssignID,//4
+	Client_Verify,
+	Client_RegisterWithServer,//6
+	Client_UnregisterWithServer,
+
+	Game_BeginGame,//8
+	Game_EndGame,
+	Game_UpdatePlayer	//10
+};
+
+// struct PieceMove {
+	
+// 	Pieces PieceType;
+// 	pos To;
+// 	pos From;
+// };
+	
+// struct PlayerDescription
+// {
+// 	uint32_t UniqueID = 0;
+// 	uint32_t Affiliation = 0;
+
+// 	uint32_t PiecesAlive = 16;
+// };
+
---
+++ include/setupServer.hpp
@@ -0,0 +1,126 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <iostream>
+#include <string>
+
+#include <QtWidgets>
+#include <QtNetwork/QHostAddress>
+#include <QDialog>
+#include <QErrorMessage>
+
+QT_BEGIN_NAMESPACE
+class QAction;
+class QDialogButtonBox;
+class QGroupBox;
+class QLabel;
+class QLineEdit;
+class QMenu;
+class QMenuBar;
+class QPushButton;
+class QTextEdit;
+QT_END_NAMESPACE
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+	Dialog()
+	{
+	    scrambler = "";
+
+	    continueButton = new QPushButton("&Continue", this);
+	    continueButton -> setAutoDefault(true);
+	    continueButton -> setMaximumWidth(70);
+	    connect(continueButton, &QPushButton::clicked, this, &Dialog::getScrambler);
+
+	    scramblerLineEdit = new QLineEdit("", this);
+	    scramblerLineEdit -> setMaxLength(10);
+	    scramblerLineEdit -> setMaximumWidth(95);
+
+	    QVBoxLayout *mainLayout = new QVBoxLayout;
+
+	    mainLayout -> addWidget(new QLabel("Enter scrambler value :"));
+	    mainLayout -> addSpacing(10);
+	    mainLayout -> addWidget(scramblerLineEdit, 0, Qt::AlignCenter);
+	    mainLayout -> addSpacing(10);
+	    mainLayout -> addWidget(continueButton, 0, Qt::AlignRight);
+
+	    setLayout(mainLayout);
+
+	    setWindowTitle(tr("Setup Scrambler"));
+	    setMinimumSize(200, 130);
+	    setMaximumSize(200, 130);
+	}
+
+signals: void exportScrambler(const std::string out);
+private:
+	QLineEdit *scramblerLineEdit;
+	QPushButton *continueButton;
+	QHostAddress ipAddress;
+	std::string  scrambler;
+
+    	void getScrambler(){
+		scrambler = scramblerLineEdit->displayText().toStdString();
+		if(scrambler.length()){
+			emit exportScrambler(scrambler);
+		}
+	}
+};
+
+
+class askMediumDialog : public QDialog 
+{
+    Q_OBJECT
+
+public:
+	askMediumDialog()
+	{
+
+	    continueButton = new QPushButton("&Continue", this);
+	    continueButton -> setAutoDefault(true);
+	    continueButton -> setMaximumWidth(70);
+	    connect(continueButton, &QPushButton::clicked, this, &askMediumDialog::getRadio);
+
+	    QVBoxLayout *mainLayout = new QVBoxLayout;
+	
+	    client = new QRadioButton("Client", this);
+	    server = new QRadioButton("Server", this);
+	    mainLayout -> addWidget(client, 50, Qt::AlignCenter);
+	    mainLayout -> addSpacing(10);
+	    mainLayout -> addWidget(server, 50, Qt::AlignCenter);
+	    mainLayout -> addSpacing(10);
+	    mainLayout -> addWidget(continueButton, 0, Qt::AlignRight);
+
+	    setLayout(mainLayout);
+
+	    setWindowTitle(tr("Setup Scrambler"));
+	    setMinimumSize(200, 130);
+	    setMaximumSize(200, 130);
+	}
+
+signals: 
+	void medium (const bool ret);
+private:
+	QRadioButton *client;
+	QRadioButton *server;
+	QPushButton *continueButton;
+	bool makeServer;
+	bool makeClient;
+
+	void getRadio(){
+		makeServer = server->isChecked();
+		makeClient = client->isChecked();
+		if(makeServer ^ makeClient){
+		
+			emit medium(makeServer);			// emit signal for choice
+		}
+		else {
+			QMessageBox errorBox;
+			errorBox.setText("Make a choice.");
+			errorBox.exec();
+		}
+	}
+};
+#endif // DIALOG_H
